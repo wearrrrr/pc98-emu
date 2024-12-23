b71efa41776dbc7afac5c12b090c147b64dbee3b
wearrrrr <contact@wearr.dev>
2024-12-23 15:15:55 -0600

    Add zero318's 8086_cpu

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,29 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-#include "emu/cpu.h"
+#include "emu/cpu/8086_cpu.h"
 
 int main(int argc, char* argv[]) {
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
 
-    CPU cpu = CPU();
-
-    uint8_t program[] = {
-        0xB8, 0x00, 0x01, // MOV AX, 0x0100
-        0x50, // PUSH AX
-        0x59, // POP CX
-        0x05, 0x00, 0x01, // ADD AX, 0x0100
-        0x08, 0xE7, // OR AL, BL
-        0xB8, 0xFF, 0xFF, // MOV AX, 0xFFFF
-        // Add 0x02 to AX
-        0x83, 0xC0, 0x02, // ADD AX, 0x02
-
-        0xF4, // HLT
-    };
-
-    cpu.load(program, sizeof(program));
-
-    while (!cpu.HLT) {
-        cpu.clock();
+    // Read BIOS.ROM into memory
+    FILE* bios = fopen("BIOS.ROM", "rb");
+    if (bios == NULL) {
+        printf("Failed to open BIOS.ROM\n");
+        return 1;
     }
 
-    printf("%s", cpu.GetRegisterState().c_str());
+    fseek(bios, 0, SEEK_END);
+    size_t bios_size = ftell(bios);
+    fseek(bios, 0, SEEK_SET);
+    for (size_t i = 0; i < bios_size; i++) {
+        mem.write(i, fgetc(bios));
+    }
+
+    execute_z86();
+
+    // printf("%s", cpu.GetRegisterState().c_str());
 
     // if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     //     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
