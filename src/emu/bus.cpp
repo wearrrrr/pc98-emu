d7ebe013ed3634c56bb36579a86580765380a0c0
wearrrrr <contact@wearr.dev>
2024-12-24 00:00:56 -0600

    clear out bus.cpp and bus.h, this will need to be a much more in depth abstraction very soon.

--- src/emu/bus.cpp
+++ src/emu/bus.cpp
@@ -1,11 +1,3 @@
 #include <cstdint>
 
 #include "bus.h"
-
-void Bus::cpuWrite(uint16_t addr, uint8_t data) {
-    memory.write(addr, data);
-};
-
-uint16_t Bus::cpuRead(uint16_t addr, bool readOnly) {
-    return memory.read(addr);
-};
--- src/emu/bus.h
+++ src/emu/bus.h
@@ -4,9 +4,8 @@
 
 class Bus {
     public:
-        void cpuWrite(uint16_t addr, uint8_t data);
-        uint16_t cpuRead(uint16_t addr, bool readOnly = false);
+
 
     private:
-        Mem memory;
+        
 };
